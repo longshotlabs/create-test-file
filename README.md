927255f18388f471294ad07ba2ed1522b2793472
Eric Dobbertin <eric@dairystatedesigns.com>
2021-04-13 11:48:27 -0500

    new version
    dependency updates, additional files extensions

--- README.md
+++ README.md
@@ -4,9 +4,25 @@ Adds a "Create Test File" option to the menu that shows when you right-click a f
 
 ## Features
 
-When you right-click a `.js` or `.jsx` file and choose "Create Test File", it will create a matching `.test.js` or `.test.jsx` file in the same folder.
+When you right-click a `.js` or `.jsx` or `.ts` or `.tsx` or `.mjs` file and choose "Create Test File", it will create a matching `.test.<extension>` file in the same folder.
 
-When you right-click a folder containing `.js` or `.jsx` files and choose "Create Test File", it will create a matching `.test.js` or `.test.jsx` file for every file in that folder.
+When you right-click a folder containing such files and choose "Create Test File", it will create a matching `.test.<extension>` file for every file in that folder.
+
+## Install
+
+Clone this repo, change to the created directory, and then:
+
+```sh
+code --install-extension create-test-file-1.1.0.vsix
+```
+
+## Install From Local (if you made changes)
+
+```sh
+npm install -g vsce
+vsce package
+code --install-extension create-test-file-1.1.0.vsix
+```
 
 ## Requirements
 
@@ -18,6 +34,6 @@ None
 
 ## Questions
 
-*Can I configure it to use `.tests.` or `.spec.` or __?*
+_Can I configure it to use `.tests.` or `.spec.` or \_\_?_
 
 No
--- src/createTestFile.ts
+++ src/createTestFile.ts
@@ -1,31 +1,40 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-export default function createTestFile(sourceFilePath: string, shouldOpen: boolean = true) {
-  if (sourceFilePath.indexOf('.test.') > -1) return;
+export default function createTestFile(
+  sourceFilePath: string,
+  shouldOpen: boolean = true
+) {
+  if (sourceFilePath.indexOf(".test.") > -1) return;
 
   const isDirectory = fs.lstatSync(sourceFilePath).isDirectory();
   if (isDirectory) {
-    fs.readdirSync(sourceFilePath).forEach(file => {
+    fs.readdirSync(sourceFilePath).forEach((file) => {
       createTestFile(path.join(sourceFilePath, file), false);
     });
     return;
   }
 
-  let testFilePath;
-  if (sourceFilePath.endsWith('.js')) {
+  let testFilePath: string;
+  if (sourceFilePath.endsWith(".js")) {
     testFilePath = `${sourceFilePath.slice(0, -3)}.test.js`;
-  } else if (sourceFilePath.endsWith('.jsx')) {
+  } else if (sourceFilePath.endsWith(".jsx")) {
     testFilePath = `${sourceFilePath.slice(0, -4)}.test.jsx`;
+  } else if (sourceFilePath.endsWith(".ts")) {
+    testFilePath = `${sourceFilePath.slice(0, -3)}.test.ts`;
+  } else if (sourceFilePath.endsWith(".tsx")) {
+    testFilePath = `${sourceFilePath.slice(0, -4)}.test.tsx`;
+  } else if (sourceFilePath.endsWith(".mjs")) {
+    testFilePath = `${sourceFilePath.slice(0, -4)}.test.mjs`;
   } else {
     return;
   }
 
-  fs.writeFileSync(testFilePath, '');
+  fs.writeFileSync(testFilePath, "");
 
   if (shouldOpen) {
-    vscode.workspace.openTextDocument(testFilePath).then(doc => {
+    vscode.workspace.openTextDocument(testFilePath).then((doc) => {
       vscode.window.showTextDocument(doc);
     });
   }
